9a1cb86749e7c1470b3f270045ec955db46c4e5b
Gunika2002 <90132300+Gunika2002@users.noreply.github.com>
2022-07-18 17:17:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine","ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+var items=["Buy Food","Cook Food","Eat"];
+
+app.get("/",function(req,res) {
+    var today = new Date();
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+
+    var day = today.toLocaleDateString("en-US",options);
+
+    res.render("list",{whichDay: day, newListItem: items});
+})
+
+app.post("/", function(req,res) {
+    var newListItem=req.body.newItem;
+    items.push(newListItem);
+    res.redirect("/");
+})
+
+
+app.listen(3000,function() {
+    console.log("Server working");
+})
